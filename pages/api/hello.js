1bb430e1b78735e6c276f9f51c9172c9728916b2
Diego Fernando Alayon <dfar.41@gmail.com>
2022-06-28 21:38:23 -0500

    agregando mas usuarios

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const usuarios={
-  name:"user1"
-}
+const usuarios=[
+  {name:"user1"},
+  {name:"user2"},
+  {name: 'user3'},
+  {
+    name: 'user4'
+  },
+  {name: 'user5' 
+},
+]
 
 export default function handler(req, res) {
   res.status(200).json(usuarios)
