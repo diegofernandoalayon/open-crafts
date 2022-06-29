67810ba59db54f3c9d4f9583f5adcdb2347e2e60
yordaney <yordan_11_1@outlook.es>
2022-06-28 21:18:45 -0500

    cambiando usuario

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const usuarios={
+  name:"user1"
+}
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Pedro casas' })
+  res.status(200).json(usuarios)
+
 }
