29bd7c95d074b9da431c0948c41ee86c5b78144b
Diego Fernando Alayon <dfar.41@gmail.com>
2022-06-28 21:52:31 -0500

    agregando y configurando Standard (esLint) en el proyecto

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,17 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const usuarios=[
-  {name:"user1"},
-  {name:"user2"},
-  {name: 'user3'},
-  {
-    name: 'user4'
-  },
-  {name: 'user5' 
-},
+const usuarios = [
+  { name: 'user1' },
+  { name: 'user2' },
+  { name: 'user3' },
+  { name: 'user4' },
+  { name: 'user5' }
 ]
 
-export default function handler(req, res) {
+export default function handler (req, res) {
   res.status(200).json(usuarios)
-
 }
