67397ca3d8737ece6cbe06d97c99e4c1ea3e6c78
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-14 21:10:49 -0500

    agregando pagina Docs para futura documentacion

---
+++ pages/docs/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+const Docs = () => {
+  return (
+    <>
+      <h1>Docs</h1>
+      <ul>
+        <li>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+        </li>
+      </ul>
+    </>
+  )
+}
+export default Docs
